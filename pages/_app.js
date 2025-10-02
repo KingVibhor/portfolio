abbac49e517deae7769ec92d1f9148aa8883309a
King-Vibhor <vibhorkumbhare2803@gmail..com>
2025-10-02 19:22:03 +0530

    Forgot to save

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="public/favicon.ico" />
         {/* If you prefer PNG, use: <link rel="icon" type="image/png" href="/favicon.png" /> */}
       </Head>
       <Component {...pageProps} />
